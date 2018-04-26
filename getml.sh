981ace0435023ff82eaefdd40351056978f1eaaf
christophernhill <cnh@mit.edu>
2018-04-26 12:04:07 -0400

    Update getml.sh

--- getml.sh
+++ getml.sh
@@ -2,4 +2,4 @@
 #
 #  Step 1 - get a list of the hosts
 #
-RUBYOPT='-W0' vagrant status | grep running | awk '{print $1}' | tee vml.txt
+RUBYOPT='-W0' vagrant status | grep running | grep -v 'running\.' | awk '{print $1}' | tee vml.txt
